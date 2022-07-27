d01aadaaf04393904408603135d4c69bae3d17fb
drovosekov <drovosekovanton@gmail.com>
2022-07-27 16:28:15 +0400

    describe additional paths on left pane in base template set dummy classes for them

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, AllPostsView, AllBloggersView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
+    path('blogs', AllPostsView.as_view(), name='blogs'),
+    path('bloggers', AllBloggersView.as_view(), name='bloggers'),
 ]
--- miniblog/views.py
+++ miniblog/views.py
@@ -4,3 +4,12 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'miniblog/index.html'
+
+
+class AllBloggersView(generic.ListView):
+    pass
+
+
+class AllPostsView(generic.ListView):
+    pass
+
