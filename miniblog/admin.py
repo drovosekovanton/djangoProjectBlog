49f423eef3b26e1dee78f4a011e2e4dcc98d7a28
drovosekov <drovosekovanton@gmail.com>
2022-07-27 16:18:40 +0400

    refactoring models add models to admin page

--- miniblog/admin.py
+++ miniblog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from miniblog.models import BlogUser, Post, Comment
 
-# Register your models here.
+
+class BloggerInline(admin.TabularInline):
+    model = BlogUser
+    verbose_name_plural = 'bloggers'
+
+
+class BloggerAdmin(admin.ModelAdmin):
+    inlines = [BloggerInline]
+
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    verbose_name_plural = 'comments'
+
+
+admin.site.register(BlogUser, BloggerAdmin)
+admin.site.register(Post)
+# TODO: tie comments with post somehow
+# admin.site.register(Comment)
--- miniblog/models.py
+++ miniblog/models.py
@@ -1,13 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    pass
+class BlogUser(models.Model):
+    """
+        Users will be added through admin form (for now)
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_author = models.BooleanField()
+    bio = models.TextField()
+    # usage shortcut
+    # u = User.objects.get(username='smith')
+    # bio = u.blogger.bio
+
+    def __str__(self):
+        return self.user.get_full_name()
 
 
 class Post(models.Model):
-    pass
+    date_published = models.DateTimeField()
+    author = models.ForeignKey(BlogUser, on_delete=models.CASCADE)
+    post_text = models.TextField()
+
+    def __str__(self):
+        return f'{self.author.user.get_full_name()}: {self.post_text[:50]}'
 
 
 class Comment(models.Model):
-    pass
+    date_published = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.get_full_name()}: {self.comment_text[:50]}'
