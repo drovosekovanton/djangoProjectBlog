77407f2f466d18f828edf8c0522a9121d4e3ea3b
drovosekov <drovosekovanton@gmail.com>
2022-08-12 08:33:00 +0400

    Masking HyperlinkedModelSerializer errors

--- miniblog/serializers.py
+++ miniblog/serializers.py
@@ -3,19 +3,19 @@ from .models import Post, Comment
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email']
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
 
 
-class CommentSerializer(serializers.HyperlinkedModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
